f6144838fd6b08433dd272e3f1fff1f4e193b9b2
Wilmer A Rubio Rojas <rubiorojasw@gmail.com>
2018-02-09 14:59:00 -0500

    Corrigiendo parametros en los modelos

--- promotions/models.py
+++ promotions/models.py
@@ -1,12 +1,16 @@
 # coding=utf-8
 from __future__ import unicode_literals
+
+from django.contrib import admin
 from django.contrib.auth.models import User
 from django import forms
 from django.forms import ModelForm
 from django.db import models
 
-
 # Create your models here.
+admin.site.unregister(User)
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
