65be77a27c8441cc6eb016a600de85736ced6b2c
Wilmer A Rubio Rojas <rubiorojasw@gmail.com>
2018-02-09 18:47:04 -0500

    Corrigiendo POST register

--- promotions/urls.py
+++ promotions/urls.py
@@ -1,10 +1,10 @@
 import django.conf.urls
 
 from . import views
-from .views import list_promotion
+from .views import list_promotion, add_user
 
 urlpatterns = [
     django.conf.urls.url(r'^$', views.home, name='home'),
     django.conf.urls.url(r'^list_promotion/$', list_promotion, name="promotions"),
-    django.conf.urls.url(r'^registerUsers/$', views.add_user, name='registerUsers'),
+    django.conf.urls.url(r'^register_users/$', views.add_user, name='register_users'),
 ]
