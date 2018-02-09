98af93d398eeb57926e8a0d15ab1d0fbd895ece0
Wilmer A Rubio Rojas <rubiorojasw@gmail.com>
2018-02-09 14:11:56 -0500

    Agregando campos para registro de usuarios

--- promotions/models.py
+++ promotions/models.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
-from django.forms import ModelForm, forms
+from django import forms
+from django.forms import ModelForm
 from django.db import models
 
 
