6d79ff050de7651a308a3db40c5705422fa7169e
Wilmer A Rubio Rojas <rubiorojasw@gmail.com>
2018-02-09 15:21:12 -0500

    Corrigiendo parametros en los modelos

--- promotions/models.py
+++ promotions/models.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 from __future__ import unicode_literals
 
-from django.contrib.auth.models import User
+
 from django import forms
+from django.contrib.auth.models import User
 from django.forms import ModelForm
 from django.db import models
 
